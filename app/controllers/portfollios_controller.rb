ff2ce7a2dc6704a116c3b974cf504b45cbf114ed
stevatech <stevan.cubrilo96@gmail.com>
2022-08-04 17:50:16 +0200

    Implemented edit and update functionality

--- app/controllers/portfollios_controller.rb
+++ app/controllers/portfollios_controller.rb
@@ -21,6 +21,24 @@ class PortfolliosController < ApplicationController
         end
     end
 
+    def edit
+        @portfolio_item = Portfollio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portfollio.find(params[:id])
+
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portfollio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfollios_path, notice: "The record was successfully updated." }
+            
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            
+          end
+        end
+    end
+
 
 end
 
