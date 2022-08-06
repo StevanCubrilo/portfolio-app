4a7f6ae02ae6c0375dfbe568549d917ccb60a6d9
stevatech <stevan.cubrilo96@gmail.com>
2022-08-06 12:25:06 +0200

    Integrated technology model

---
+++ app/models/technology.rb
@@ -0,0 +1,3 @@
+class Technology < ApplicationRecord
+  belongs_to :portfollio
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -52,3 +52,12 @@ end
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+    Portfollio.last.technologies.create!(
+        name: 'Technology #{technology}'
+        
+    )
+end
+
+puts "3 technologies created"
