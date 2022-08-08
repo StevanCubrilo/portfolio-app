28658b56edb0950128c885fd7ff6674649c70332
stevatech <stevan.cubrilo96@gmail.com>
2022-08-08 20:33:41 +0200

    Added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include DeviseWhitelist
+
+
+    #set_source nema veze sa nekom posebnom metodom, nego je ime koje mi sami odredjujemo 
+    before_action :set_source
+
+    def set_source
+        #I want you to set the session and set on something called source and set it equal to whatever params are
+        #and after we done that we would have acces to them on page, where we want
+        session[:source] = params[:q] if params[:q]
+    end
 end
